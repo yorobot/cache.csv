25beeadcb9120d07bc1f52744d176e6454f2803d
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-06-21 21:47:54 +0200

    up checksum

--- boot.rb
+++ boot.rb
@@ -1,8 +1,8 @@
 ## note: use the local version of sportdb-source gem
 $LOAD_PATH.unshift( File.expand_path( '../../../sportdb/sport.db/sportdb-formats/lib') )
 $LOAD_PATH.unshift( File.expand_path( '../../../sportdb/sport.db/sportdb-importers/lib') )
-
 require 'sportdb/importers'
+
 
 ## use (switch to) "external" datasets
 SportDb::Import.config.clubs_dir   = "../../../openfootball/clubs"
@@ -10,6 +10,9 @@ SportDb::Import.config.leagues_dir = "../../../openfootball/leagues"
 
 
 $LOAD_PATH.unshift( File.expand_path( '../../football.csv/sportdb-linters/lib') )
-
 require 'sportdb/linters'    # e.g. uses TeamSummary class
+
+
+
+
 
---
+++ cache.footballdata/footballdata.rb
@@ -0,0 +1,6 @@
+require_relative '../boot'
+
+
+$LOAD_PATH.unshift( File.expand_path( '../../../sportdb/sport.db.sources/sportdb-source-footballdata/lib') )
+require 'sportdb/source/footballdata'
+
