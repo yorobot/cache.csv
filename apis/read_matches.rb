573b1c0f9f0ffb92ae272522ae926219e53794f6
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-06-24 11:06:43 +0200

    up

--- apis/read_matches.rb
+++ apis/read_matches.rb
@@ -3,25 +3,58 @@ require 'date'
 require 'pp'
 
 
-path = './dl/competitions~~FL1~~matches-I-season~2019.json'
-# path = './dl/competitions~~PL~~matches-I-season~2019.json'
-# path = './dl/competitions~~ELC~~matches-I-season~2018.json'
-# path = './dl/competitions~~BSA~~matches-I-season~2020.json'
+require_relative '../csv'        ## pull in date_to_season helper
 
+
+
+# OUT_DIR = './o'
+OUT_DIR = '../../stage/one'
+
+
+LEAGUES = {
+  'eng.1' => 'PL',
+  'eng.2' => 'ELC',
+  'br.1'  => 'BSA',
+  'fr.1'  => 'FL1'
+}
+
+MODS = {
+  'br.1' => { 'América FC' => 'América (MG)',   # in year 2018
+            }
+}
+
+
+def convert( league:, year: )
+
+  path = "./dl/competitions~~#{LEAGUES[league.downcase]}~~matches-I-season~#{year}.json"
+  puts "path=>#{path}<"
 
 txt = File.open( path, 'r:utf-8' ) {|f| f.read }
 data = JSON.parse( txt )
 
 
+recs = []
+
 teams = Hash.new( 0 )
 
-cols = {}
-cols[:duration] = Hash.new( 0 )
-cols[:stage]    = Hash.new( 0 )
-cols[:status]   = Hash.new( 0 )
-cols[:group]    = Hash.new( 0 )
+stat = {
+  all:     { duration: Hash.new( 0 ),
+             stage:    Hash.new( 0 ),
+             status:   Hash.new( 0 ),
+             group:    Hash.new( 0 ),
 
-goals = 0
+             matches:  0,
+             goals:    0,
+           },
+  regular: { duration: Hash.new( 0 ),
+             stage:    Hash.new( 0 ),
+             status:   Hash.new( 0 ),
+             group:    Hash.new( 0 ),
+
+             matches:  0,
+             goals:    0,
+  }
+}
 
 matches = data[ 'matches']
 
@@ -49,38 +82,99 @@ matches.each do |m|
 
   score = m['score']
 
-  goals += score['fullTime']['homeTeam'].to_i  if score['fullTime']['homeTeam']
-  goals += score['fullTime']['awayTeam'].to_i  if score['fullTime']['awayTeam']
+  stat[:all][:stage][ m['stage'] ]   += 1
+  stat[:all][:group][ m['group'] ]  += 1
+  stat[:all][:duration][ score['duration'] ] += 1   ## track - assert always REGULAR
+  stat[:all][:status][ m['status'] ]  += 1
+
+  stat[:all][:matches] += 1
+  stat[:all][:goals]   += score['fullTime']['homeTeam'].to_i  if score['fullTime']['homeTeam']
+  stat[:all][:goals]   += score['fullTime']['awayTeam'].to_i  if score['fullTime']['awayTeam']
 
 
-  ft = "#{score['fullTime']['homeTeam']}-#{score['fullTime']['awayTeam']}"
-  ht = "#{score['halfTime']['homeTeam']}-#{score['halfTime']['awayTeam']}"
+  if m['stage'] == 'REGULAR_SEASON'
+    stat[:regular][:stage][ m['stage'] ]   += 1
+    stat[:regular][:group][ m['group'] ]  += 1
+    stat[:regular][:duration][ score['duration'] ] += 1   ## track - assert always REGULAR
+    stat[:regular][:status][ m['status'] ]  += 1
 
-  cols[:duration][ score['duration'] ] += 1   ## track - assert always REGULAR
-  cols[:stage][ m['stage'] ]   += 1
-  cols[:status][ m['status'] ]  += 1
-  cols[:group][ m['group'] ]  += 1
+    stat[:regular][:matches] += 1
+    stat[:regular][:goals]   += score['fullTime']['homeTeam'].to_i  if score['fullTime']['homeTeam']
+    stat[:regular][:goals]   += score['fullTime']['awayTeam'].to_i  if score['fullTime']['awayTeam']
 
-  teams[ team1 ] += 1
-  teams[ team2 ] += 1
 
-  ## e.g. "utcDate": "2020-05-09T00:00:00Z",
-  date_str = m['utcDate']
-  date = DateTime.strptime( date_str, '%Y-%m-%dT%H:%M:%SZ' )
+    teams[ team1 ] += 1
+    teams[ team2 ] += 1
 
-  print '%2s' % m['matchday']
-  print ' - '
-  print '%-24s' % team1
-  print "  #{ft} (#{ht})  "
-  print '%-24s' % team2
-  print '   '
-  print ' | '
-  ## print date.to_date  ## strip time
-  print date.to_date.strftime( '%a %b %-d %Y' )
-  print ' -- '
-  print date
-  print "\n"
-end
+
+    ## e.g. "utcDate": "2020-05-09T00:00:00Z",
+    date_str = m['utcDate']
+    date = DateTime.strptime( date_str, '%Y-%m-%dT%H:%M:%SZ' )
+
+
+    comments = ''
+    ft       = ''
+    ht       = ''
+
+    case m['status']
+    when 'SCHEDULED'
+      ft = ''
+      ht = ''
+    when 'FINISHED'
+      ft = "#{score['fullTime']['homeTeam']}-#{score['fullTime']['awayTeam']}"
+      ht = "#{score['halfTime']['homeTeam']}-#{score['halfTime']['awayTeam']}"
+    when 'AWARDED'
+      ft = "#{score['fullTime']['homeTeam']}-#{score['fullTime']['awayTeam']}"
+      ft << ' (*)'
+      ht = ''
+      comments = 'awarded'
+    when 'CANCELLED'
+      ft = '(*)'
+      ht = ''
+      comments  = 'canceled'
+    when 'POSTPONED'
+      ft = '(*)'
+      ht = ''
+      comments = 'postponed'
+    else
+      puts "!! ERROR: unsupported match status >#{m['status']}< - sorry:"
+      pp m
+      exit 1
+    end
+
+
+    recs << [m['matchday'],
+             date.to_date.strftime( '%Y-%m-%d' ),
+             team1,
+             ft,
+             ht,
+             team2,
+             comments
+            ]
+
+
+    print '%2s' % m['matchday']
+    print ' - '
+    print '%-24s' % team1
+    print '  '
+    print ft
+    print ' '
+    print "(#{ht})"    unless ht.empty?
+    print '  '
+    print '%-24s' % team2
+    print '  '
+    print comments
+    print ' | '
+    ## print date.to_date  ## strip time
+    print date.to_date.strftime( '%a %b %-d %Y' )
+    print ' -- '
+    print date
+    print "\n"
+  else
+    puts "-- skipping #{m['stage']}"
+  end
+end # each match
+
 
 season_key = if start_date.year == end_date.year
                "%4d" % start_date.year
@@ -94,7 +188,62 @@ season_key = if start_date.year == end_date.year
              end
 
 dates = "#{start_date.strftime('%b %-d')} - #{end_date.strftime('%b %-d')}"
-puts "#{season_key} (#{dates}) - #{teams.keys.size} clubs, #{matches.size} matches, #{goals} goals"
+
+buf = ''
+buf << "#{season_key} (#{dates}) - "
+buf << "#{teams.keys.size} clubs, "
+buf << "#{stat[:regular][:matches]} matches, "
+buf << "#{stat[:regular][:goals]} goals"
+buf << "\n"
+
+puts buf
+
+
+   File.open( './logs.txt', 'a:utf-8' ) do |f|
+     f.write "#{league} =============\n"
+     f.write buf
+   end
+
+
+# recs = recs.sort { |l,r| l[1] <=> r[1] }
+## reformat date / beautify e.g. Sat Aug 7 1993
+recs.each { |rec| rec[1] = Date.strptime( rec[1], '%Y-%m-%d' ).strftime( '%a %b %-d %Y' ) }
+
+headers = [
+  'Matchday',
+  'Date',
+  'Team 1',
+  'FT',
+  'HT',
+  'Team 2',
+  'Comments'
+]
+
+## note: change season_key from 2019/20 to 2019-20  (for path/directory!!!!)
+Cache::CsvMatchWriter.write( "./#{OUT_DIR}/#{season_key.tr('/','-')}/#{league.downcase}.csv",
+                             recs,
+                             headers: headers )
+
+
 
 pp teams
-pp cols
+pp stat
+end   # method convert
+
+
+
+
+
+convert( league: 'ENG.1', year: 2018 )
+convert( league: 'ENG.1', year: 2019 )
+
+convert( league: 'ENG.2', year: 2018 )
+convert( league: 'ENG.2', year: 2019 )
+
+convert( league: 'FR.1', year: 2018 )
+convert( league: 'FR.1', year: 2019 )
+
+convert( league: 'BR.1', year: 2018 )
+convert( league: 'BR.1', year: 2019 )
+convert( league: 'BR.1', year: 2020 )
+
---
+++ apis/summary.rb
@@ -0,0 +1,19 @@
+require_relative '../boot'
+
+
+
+DATAFILES_DIR = '../../stage/one'
+
+team_buf,   team_errors   = SportDb::TeamSummary.build( DATAFILES_DIR, start: '1989' )
+
+# OUT_DIR = DATAFILES_DIR
+OUT_DIR  = './o'   ## for (local) debugging
+
+File.open( "#{OUT_DIR}/SUMMARY.md", 'w:utf-8' )  { |f| f.write( team_buf ) }
+
+
+puts "#{team_errors.size} error(s) - teams:"
+pp team_errors
+
+puts 'bye'
+
