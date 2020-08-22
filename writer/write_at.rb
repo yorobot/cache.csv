e746c548395073e2014b8ca49e5f7a1d159dc4ee
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-08-22 18:15:03 +0200

    up

--- writer/write_at.rb
+++ writer/write_at.rb
@@ -3,11 +3,11 @@ require_relative './write_utils'
 
 
 
-write_worker( 'at.1', '2019/20', source: 'two/o' )
+# write_worker( 'at.1', '2019/20', source: 'two/o' )
 # write_worker( 'at.1', '2018/19', source: 'tmp/o' )
 
 
-# write_worker( 'at.2', '2019/20', source: 'two/o' )
+write_worker( 'at.2', '2019/20', source: 'two/o' )
 
 
 puts "bye"
