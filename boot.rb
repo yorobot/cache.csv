a8e043f4d1ecf3a9d08684f1bff569b3becada69
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-09-15 18:00:53 +0200

    up boot

--- boot.rb
+++ boot.rb
@@ -1,9 +1,9 @@
 ## hack: use "local" dev monoscript too :-) for now
-$LOAD_PATH.unshift( 'C:/Sites/yorobot/cache.csv/monos/lib' )
+$LOAD_PATH.unshift( 'C:/Sites/rubycoco/monos/lib' )
 
 ## note: use the local version of sportdb-source gem
-require 'mono/sportdb'
-Mono.setup   # setup load path
+require 'sportdb/setup'
+SportDb::Boot.setup   # setup load path
 
 
 
@@ -24,14 +24,14 @@ require 'sportdb/linters'    # e.g. uses TeamSummary class
 
 
 ## use (switch to) "external" datasets
-SportDb::Import.config.clubs_dir   = "#{Mono.root}/openfootball/clubs"
-SportDb::Import.config.leagues_dir = "#{Mono.root}/openfootball/leagues"
+SportDb::Import.config.clubs_dir   = "#{SportDb::Boot.root}/openfootball/clubs"
+SportDb::Import.config.leagues_dir = "#{SportDb::Boot.root}/openfootball/leagues"
 
 
 
 ####
 # for testing
 if __FILE__ == $0
-  puts "Mono.root=>#{Mono.root}<"
+  puts "SportDb::Boot.root=>#{SportDb::Boot.root}<"
   puts "bye"
 end
