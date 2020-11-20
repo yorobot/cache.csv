08036acc6f2810e6b7b92205fbc0798072a971d8
Gerald Bauer <gerald.bauer@gmail.com>
2020-11-20 19:35:55 +0100

    up

--- fbref/convert.rb
+++ fbref/convert.rb
@@ -1,42 +1,17 @@
 $LOAD_PATH.unshift( 'C:/Sites/yorobot/sport.db.more/webget-football/lib' )
-require 'webget/football'
+$LOAD_PATH.unshift( 'C:/Sites/yorobot/sport.db.more/football-sources/lib' )
+require 'football/sources'
 
 
-
-require_relative 'build'
-
-
-module Fbref
-  def self.convert( league:, season: )
-    page = Page::Schedule.from_cache( league: league,
-                                      season: season )
-
-    puts page.title
-
-    rows = page.matches
-    recs = build( rows, league: league, season: season )
-    ## pp rows
-
-    ## reformat date / beautify e.g. Sat Aug 7 1993
-    recs.each { |rec| rec[2] = Date.strptime( rec[2], '%Y-%m-%d' ).strftime( '%a %b %-d %Y' ) }
-
-
-    recs, headers = vacuum( recs )
-
-    pp recs[0..2]
-
-    season = Season.parse( season )
-    path = "o/#{league}_#{season.to_path}.csv"
-    puts "write #{path}..."
-    Cache::CsvMatchWriter.write( path, recs, headers: headers )
-  end
-end # module Fbref
 
 
 # Fbref.convert( league: 'jp.1', season: '2019' )
 # Fbref.convert( league: 'mx.1', season: '2019/20' )
 
 # Fbref.convert( league: 'at.1', season: '2018-19' )
+
+
+# Fbref.config.convert.out_dir = './o2'
 
 # convert all configured
 
