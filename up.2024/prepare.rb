3c6e1462c7934ad1704ccbc06e326f587ac00556
Gerald Bauer <gerald.bauer@gmail.com>
2024-05-20 21:53:15 +0200

    up

--- up.2024/prepare.rb
+++ up.2024/prepare.rb
@@ -14,10 +14,13 @@ Webget.config.sleep  = 11    ## max. 10 requests/minute
 
 # Footballdata.schedule( league: 'eng.1', season: '2023/24' )
 # Footballdata.schedule( league: 'de.1',  season: '2023/24' )
+# Footballdata.schedule( league: 'fr.1',  season: '2023/24' )
 
 # note:  it.1 2023/2024 ends jul/2 !!!
-# Footballdata.schedule( league: 'it.1',  season: '2023/24' )
-# Footballdata.schedule( league: 'fr.1',  season: '2023/24' )
+Footballdata.schedule( league: 'it.1',  season: '2023/24' )
+
+# note: es.1 2023/2024 end may/26 !!!
+Footballdata.schedule( league: 'es.1',  season: '2023/24' )
 
 
 #####
@@ -31,8 +34,9 @@ SEASONS = %w[2023/24 2022/23 2021/22 2020/21]
 DATASETS = [
  ['eng.1',   SEASONS],  
  ['de.1',    SEASONS],
+ ['es.1',    SEASONS],
  ['it.1',    SEASONS - %w[2023/24]],
- ['fr.1',    SEASONS] 
+ ['fr.1',    SEASONS],
 ]
 
 
@@ -49,16 +53,14 @@ puts "bye"
 
 
 __END__
+
+double check for more (*) awared, cancelled, and such!!!
+
+
 !! check for 2020/21 in it.1
   1,Sat Sep 19 2020,Hellas Verona FC,3-0 (*),,AS Roma,awarded
 
   
-
-!! ERROR - no club match found for >AC Monza<
-1 error(s) - teams:
-["AC Monza > Italy"]
-
-
 !! ERROR: unsupported match status >IN_PLAY< - sorry:
 "utcDate"=>"2024-05-20T16:30:00Z",
 "status"=>"IN_PLAY",
@@ -67,4 +69,10 @@ __END__
 note - IN_PLAY (same as playing now!!! LIVE)
 ##  retry when match ended!!!!
 
+!! ERROR: unsupported match status >PAUSED< - sorry:
+## - why paused??
+{"area"=>{"id"=>2114, "name"=>"Italy", "code"=>"ITA", "flag"=>"https://crests.football-data.org/784.svg"},
+ "utcDate"=>"2024-05-20T18:45:00Z",
+ "status"=>"PAUSED",
+ "matchday"=>37,
 
