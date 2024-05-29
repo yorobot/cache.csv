c4253127dc281e33721d8eaa57b3549b34bd516f
Gerald Bauer <gerald.bauer@gmail.com>
2024-05-29 16:30:55 +0200

    up

--- up.squads/top.rb
+++ up.squads/top.rb
@@ -1,3 +1,18 @@
+##
+# todos/fix
+#
+#   date format add  "-" for empty!!
+#    ! WARN - no date of birth (or wrong format)
+#    e.g "Date of Birth"=>"-",
+#
+#   add date format - year (with single digit) 
+#    e.g.  "Date of Birth"=>"25-08-5", or
+#   
+#  name lookup
+#    for fr.1 - add  monaco !!
+#    for 
+
+
 require_relative 'helper'   ## (shared) boot helper
 
 
@@ -67,14 +82,29 @@ def pp_player( rec )
     buf << "   "
 
     dob_str = rec['Date of Birth']
+
+  ## fix for  Mike Penders (BEL)
+  if rec['Name'] == 'Mike Penders' && rec['Nat'] == 'BEL'
+     ## "Date of Birth"=>"31-06-05",
+     dob_str = '31-07-05'  #  is July (not June) - June 31st does NOT exist!!  
+  end
+
     ## note: MUST parse by our own (in ruby year 65 => 2065)
     ## 24-06-99
     ## assert date format
     dob = if dob_str.match( /^\d{2}-\d{2}-\d{2}$/ )
             dob_i = dob_str.split( '-').map { |str| str.strip.to_i(10) }
-            Date.new( dob_i[2] < 20 ? 2000+dob_i[2] : 1900+dob_i[2],
-                    dob_i[1],
-                    dob_i[0] )
+            begin
+              Date.new( dob_i[2] < 20 ? 2000+dob_i[2] : 1900+dob_i[2],
+                      dob_i[1],
+                      dob_i[0] )
+            rescue => ex
+              puts "! WARN - error date of birth:"
+              pp ex
+              pp dob_str
+              pp rec
+              nil            
+            end
           else
               puts "! WARN - no date of birth (or wrong format):"
               pp dob_str
@@ -100,34 +130,84 @@ end
 def convert_league(  outdir:,
                      league:,
                      season: )
-
+                     
 league_page = Footballsquads.league( league: league, season: season )
 pp league_page.title
 
+## find league rec
+leagues = SportDb::Import.catalog.leagues
+league_rec   = leagues.find!( league )
+
+
 league_page.each_team do |team_page|
-   pp team_page.title
+   # pp team_page.title
 
    team_name         =  team_page.team_name
-   pp team_name
+   # pp team_name
    team_name_official = team_page.team_name_official
-   pp team_name_official
+   # pp team_name_official
 
-   current, past =  team_page.players
+##
+#  map team name
+#  
+    # try short name first
+    clubs =  SportDb::Import.catalog.clubs
 
-   puts "current (#{current.size}):"
-   pp current
-   puts "past (#{past.size}):"
-   pp past  
+    m1 = clubs.match_by( name: team_name,           country: league_rec.country.key )
+    m2 = clubs.match_by( name: team_name_official,  country: league_rec.country.key )
+
+   team_name_short =  
+    if m1.size == 1 && m2.size == 1
+       puts "OK  #{team_name}  -  #{team_name_official}  =>  #{m1[0].name}"
+
+       m1[0].name   ## use "canonical name"
+    else
+      ## report 
+      if m1.size == 0 && m2.size == 0
+        puts
+        puts "!!  no match for team in  #{league_rec.country.name} - #{league_rec.name}:"
+        puts "    #{team_name}"
+        puts "    #{team_name_official}"
+     else
+        puts "!!  missing match for team in  #{league_rec.country.name} - #{league_rec.name}:"
+        if m1.size == 0
+            puts "   !!  #{team_name}"
+        elsif m1.size == 1
+            puts "   OK  #{team_name}"
+        else
+            puts "   !!  #{team_name}  - too many matches - #{m1.size}:"
+            pp m1
+        end
+        if m2.size == 0
+          puts "   !!  #{team_name_official}"
+        elsif m2.size == 1
+          puts "   OK  #{team_name_official}"
+        else
+          puts "   !!  #{team_name_official}  - too many matches - #{m2.size}:"
+          pp m2
+        end
+
+        ## use org name  with official name as comment
+        ##   -- log warning!! to errors.txt or logs.txt or such!!!
+        "#{team_name_official}    # #{team_name}"
+    end
+  end
+
+  current, past =  team_page.players
+
+   # puts "current (#{current.size}):"
+   # pp current
+   # puts "past (#{past.size}):"
+   # pp past  
    
    ## e.g. Brighton & Hove Albion => brighton_hove_albion
    slug = unaccent( team_name ).downcase.gsub( /[^a-z0-9 ]/, '').gsub( /[ ]+/, '_' )
    season_slug = Season( season ).to_path
    path = "#{outdir}/#{season_slug}/squads/#{slug}.txt"
 
-
 ## note - use official (long) team name - add (short) team name as comment   
 buf = ""
-buf << "= #{team_name_official}    # #{team_name}\n\n"
+buf << "= #{team_name_short}\n\n"
 buf << pp_players( sort_players(current))
    
 write_text( path, buf )
@@ -141,8 +221,8 @@ end  # method convert_league
 
 
 
-=begin
-DATASETS = [
+
+DATASETS_TOP = [
   ['eng.1',   %w[2023/24]],
   ['de.1',    %w[2023/24]],
   ['es.1',    %w[2023/24]],
@@ -151,16 +231,17 @@ DATASETS = [
 
   ['at.1',    %w[2023/24]],
 ]
-=end
 
 
 # try some more
-DATASETS = [
+DATASETS_MORE = [
 #  ['sco.1',  %w[2023/24]], 
 #  ['pt.1',  %w[2023/24]],
 #  ['nl.1',   %w[2023/24]],
 
-#  ['be.1',  %w[2023/24]],
+ ['be.1',  %w[2023/24]],
+
+=begin
   ['tr.1',  %w[2023/24]],
   ['gr.1',  %w[2023/24]],
 
@@ -177,13 +258,17 @@ DATASETS = [
   ['es.2',    %w[2023/24]],
   ['fr.2',    %w[2023/24]],
   ['it.2',    %w[2023/24]],
-
+=end
 ]
 
-pp DATASETS
+pp DATASETS_TOP
+pp DATASETS_MORE
 
 
-repos  = find_repos( DATASETS )
+# datasets = DATASETS_TOP
+datasets = DATASETS_MORE
+
+repos  = find_repos( datasets )
 pp repos
 
 
@@ -206,7 +291,6 @@ Webcache.root = '../../../cache'  ### c:\sports\cache
 pp Webcache.root
 
 
-datasets = DATASETS
 datasets.each_with_index do |(league_key, seasons),i|
  
   league = Writer::LEAGUES[ league_key ]
